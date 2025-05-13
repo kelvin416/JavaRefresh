8296e03f4d6dc9e5f02be3f08051bbf15dcbb8bd
Kelvin Rotich <kelvinrotich416@gmail.com>
2025-05-13 16:44:23 +0300

    arrays

--- src/labs_examples/arrays/labs/Exercise_01.java
+++ src/labs_examples/arrays/labs/Exercise_01.java
@@ -1,6 +1,7 @@
 package labs_examples.arrays.labs;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  * Arrays calculator
@@ -13,5 +14,26 @@ import java.util.Arrays;
  */
 
 public class Exercise_01 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+
+        int[] numbers = new int[10];
+        System.out.println("Enter 10 numbers: ");
+
+        for (int i = 0; i < numbers.length; i++){
+            System.out.println("Number " + (i + 1) + ": ");
+        }
+
+        for (int i = 0; i < numbers.length; i++){
+            sum += numbers[i];
+        }
+
+        int avg = sum / numbers.length;
+
+        System.out.println("\n Sum of the numbers: " + sum);
+        System.out.println("\n Avg of the numbers: " + avg);
+    }
 
 }
