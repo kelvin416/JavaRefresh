ef7e8c4f1de5cf775f7dbb8153e07f2a646f3828
Kelvin Rotich <kelvinrotich416@gmail.com>
2025-06-30 20:48:54 +0300

    packages

---
+++ src/labs_examples/packages/labs/package1/Package1Example.java
@@ -0,0 +1,4 @@
+package labs_examples.packages.labs.package1;
+
+public class Package1Example {
+}
---
+++ src/labs_examples/packages/labs/package2/Package2Example.java
@@ -0,0 +1,4 @@
+package labs_examples.packages.labs.package2;
+
+public class Package2Example {
+}
