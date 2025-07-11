0d96cb7be6457123162719443ceba45a1740e58c
Kelvin Rotich <kelvinrotich416@gmail.com>
2025-07-11 18:18:58 +0300

    Enums

--- src/labs_examples/enumerations/labs/Exercise_01.java
+++ src/labs_examples/enumerations/labs/Exercise_01.java
@@ -8,3 +8,71 @@ package labs_examples.enumerations.labs;
  *      of this enum from a separate class.
  */
 
+enum DaysOfWeek {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
+
+class DaysController{
+
+    private DaysOfWeek daysOfWeek;
+
+    public DaysController(DaysOfWeek daysOfWeek){
+        this.daysOfWeek = daysOfWeek;
+    }
+
+    public boolean isWeekend(){
+        return this.daysOfWeek == DaysOfWeek.SUNDAY;
+    }
+
+    public static void main(String[] args) {
+        DaysController daysController = new DaysController(DaysOfWeek.SUNDAY);
+        System.out.println("Is it a weekend? " +daysController.isWeekend() );
+    }
+}
+
+// Below is enums with value usage
+
+enum PizzaSize{
+    SMALL(8),
+    MEDIUM(12),
+    LARGE(16),
+    EXTRA_LARGE(20);
+
+    private final int DIAMETERININCHES;
+
+    PizzaSize(int diameter) {
+        this.DIAMETERININCHES = diameter;
+    }
+}
+
+class PizzaSizeController{
+    private PizzaSize pizzaSize;
+
+    public PizzaSizeController(PizzaSize pizzaSize) {
+        this.pizzaSize = pizzaSize;
+    }
+
+    public PizzaSize getPizzaSize() {
+        return pizzaSize;
+    }
+
+    public void setPizzaSize(PizzaSize pizzaSize) {
+        this.pizzaSize = pizzaSize;
+    }
+
+    public void displaySize(int pizzaSize){
+        System.out.println("Select size: " + pizzaSize);
+        System.out.println("Diameter in inches: " + getPizzaSize());
+    }
+
+    public static void main(String[] args) {
+        PizzaSizeController order = new PizzaSizeController(PizzaSize.MEDIUM);
+        order.displaySize(20);
+    }
+}
