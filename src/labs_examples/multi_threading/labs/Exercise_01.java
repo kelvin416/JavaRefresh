2ea47b64142e72bcbe3cf3a84591e8485e7b3788
Kelvin Rotich <kelvinrotich416@gmail.com>
2026-06-10 14:00:29 +0300

    MultiThreading

--- src/labs_examples/multi_threading/labs/Exercise_01.java
+++ src/labs_examples/multi_threading/labs/Exercise_01.java
@@ -8,4 +8,40 @@ package labs_examples.multi_threading.labs;
  *
  */
 
+class Student{
+    public static void main(String[] args) {
+
+        System.out.println("....Thread starting");
+
+        StudentThread studentThread = new StudentThread("James");
+        StudentThread studentThread2 = new StudentThread("Cerise");
+        StudentThread studentThread3 = new StudentThread("Charles");
+
+        System.out.println("Thread Finishing....");
+    }
+}
+
+class StudentThread implements Runnable{
+
+    Thread thread;
+
+    StudentThread(String name){
+        thread = new Thread(this, name);
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+        System.out.println(thread.getName() + " starting school");
+        try {
+            for (int sem = 1; sem <= 4; sem++){
+                Thread.sleep(600);
+                System.out.println(thread.getName() + " goes to school in sem " + sem);
+            }
+        } catch (InterruptedException e) {
+            System.out.println(thread.getName() + " interrupted");
+        }
+    }
+}
+
 
